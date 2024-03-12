39347ea2b1d119591a93a6dcb10faa8b3cc4e312
vehe538 <juro.mesik@gmail.com>
2024-03-13 00:40:46 +0100

    EDITED TO TEST xtf2

--- test_xtf.sh
+++ test_xtf.sh
@@ -28,13 +28,13 @@ rm -rf diff
 run_test() {
     local expected_output=${1}   
     local args=("${@:2}")
-    echo -n -e "$test_count. Running ./xtf"
+    echo -n -e "$test_count. Running ./xtf2"
     for arg in "${args[@]}"; do
         echo -n " \"$arg\""
     done
     echo ""
 
-    local actual_output=$(./xtf "${args[@]}")
+    local actual_output=$(./xtf2 "${args[@]}")
     
     if [[ "$actual_output" == "$expected_output" ]]; then
         echo -e "${GREEN} [OK] ${NORMAL}"
